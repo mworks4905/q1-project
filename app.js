dab0a83bed75f6a125e99a32bf1d25c99ec8ea60
Matt Works <mworks4905@gmail.com>
2016-08-30 14:03:43 -0600

    Timing for slingshot works

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ var mouse = MouseConstraint.create(engine,{
 
 // create two boxes and a ground
 var boxA = Bodies.rectangle(600, 570, 40, 80);
-var boxB = Bodies.rectangle(625, 50, 160, 20, {inertia: 100000});
-var boxC = Bodies.rectangle(650, 400, 40, 80);
-var boxD = Bodies.rectangle(660, 300, 25, 100);
+// var boxB = Bodies.rectangle(625, 50, 160, 20, {inertia: 100000});
+// var boxC = Bodies.rectangle(650, 400, 40, 80);
+// var boxD = Bodies.rectangle(660, 300, 25, 100);
 var ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
 // var car = Composites.car(100,100,40,40,30)
 
@@ -46,18 +46,58 @@ var rock = Bodies.polygon(170, 450, 12, 20),
     });
 
 // add all of the bodies to the world
-World.add(engine.world, [mouse, boxA, boxB, boxC, boxD, ground, car, rock, elastic]);
+World.add(engine.world, [mouse, boxA, ground, rock, elastic]);
 
-Events.on(engine,'tick', function(event) {
-  if(rock.position.x > 190 || rock.position.y < 430){
-    rock = Bodies.polygon(170, 450, 12, 20);
-           World.add(engine.world, rock);
-           elastic.bodyB = rock;
-           console.log('You shot a rock!')
+// var event1 = Events.on(engine,'mouseup'/*'tick'*/, function(event) {
+//   if(rock.position.x > 190 || rock.position.y < 430){
+//     return true;
+//     // rock = Bodies.polygon(170, 450, 12, 20);
+//     //        World.add(engine.world, rock);
+//     //        elastic.bodyB = rock;
+//     //        console.log('You shot a rock!')
+//   }
+// })
+
+var event2 = Events.on(mouse, 'mouseup', function(event) {
+  var mousePosition = event.mouse.position;
+  var currentTime = engine.timing.timestamp
+
+  console.log(currentTime);
+  console.log('mouseup at ' + mousePosition.x + ' ' + mousePosition.y);
+
+  if(mousePosition.x < 250 && mousePosition.y > 315){
+    setTimeout(createRock, 0125)
+
   }
-
-
 })
+// var shootRock = function (condition1, condition2){
+function createRock(){
+  rock = Bodies.polygon(170, 450, 12, 20);
+         World.add(engine.world, rock);
+         elastic.bodyB = rock;
+         console.log('You shot a rock!')
+
+}
+//   if(condition1 && condition2){
+//   rock = Bodies.polygon(170, 450, 12, 20);
+//   World.add(engine.world, rock);
+//   elastic.bodyB = rock;
+//   console.log('You shot a rock!')
+//   }
+// }
+//
+// Event.on(engine,'tick', shootRock(event1, event2))
+
+
+
+
+
+Events.trigger(engine, 'tick', function(event){
+  if(boxA.position.x > 600){
+    alert('You moved the box!');
+  }
+})
+
 
 
 // run the engine

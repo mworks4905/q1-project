a459282910ab4f4c736b7d258db5a240e5d20fec
Matt Works <mworks4905@gmail.com>
2016-08-30 11:19:17 -0600

    Slingshot works

--- app.js
+++ app.js
@@ -5,16 +5,9 @@ var Engine = Matter.Engine,
     World = Matter.World,
     Bodies = Matter.Bodies,
     MouseConstraint = Matter.MouseConstraint,
-    Composites = Matter.Composites;
-
-// // create an Engine
-// var engine = Engine.create();
-//
-// // create a renderer
-// var render = Render.create({
-//   element: document.body,
-//   engine: engine
-// });
+    Composites = Matter.Composites,
+    Constraint = Matter.Constraint,
+    Events = Matter.Events;
 
 // create a Matter.js engine
 var engine = Engine.create(document.body, {
@@ -22,28 +15,50 @@ var engine = Engine.create(document.body, {
     options: {
       showAngleIndicator: true,
       showVelocity: true,
-      wireframes: true
+      wireframes: false,
+      showPositions: true
     }
   }
 });
 
-var mouseConstraint = MouseConstraint.create(engine);
-World.add(engine.world, mouseConstraint);
-
-var car = Composites.car(100,100,40,40,30)
+var mouse = MouseConstraint.create(engine,{
+   constraint: { stiffness: 1 }//how much an obj adhears to the mouse
+ });
 
 // create two boxes and a ground
 var boxA = Bodies.rectangle(600, 570, 40, 80);
-var boxB = Bodies.rectangle(625, 50, 160, 20);
+var boxB = Bodies.rectangle(625, 50, 160, 20, {inertia: 100000});
 var boxC = Bodies.rectangle(650, 400, 40, 80);
 var boxD = Bodies.rectangle(660, 300, 25, 100);
 var ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
+// var car = Composites.car(100,100,40,40,30)
+
+var rock = Bodies.polygon(170, 450, 12, 20),
+    anchor = { x: 170, y: 450 },
+    elastic = Constraint.create({
+        pointA: anchor,
+        bodyB: rock,
+        stiffness: 0.1,
+        render: {
+            lineWidth: 5,
+            strokeStyle: '#dfa417'
+        }
+    });
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, boxC, boxD, ground, car]);
+World.add(engine.world, [mouse, boxA, boxB, boxC, boxD, ground, car, rock, elastic]);
+
+Events.on(engine,'tick', function(event) {
+  if(rock.position.x > 190 || rock.position.y < 430){
+    rock = Bodies.polygon(170, 450, 12, 20);
+           World.add(engine.world, rock);
+           elastic.bodyB = rock;
+           console.log('You shot a rock!')
+  }
+
+
+})
+
 
 // run the engine
 Engine.run(engine);
-
-// run the renderer
-// Render.run(render);
